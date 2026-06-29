a45e4c6a84e4072cf09244bc129107f7c79a2690
Trexdev2 <nealjackson0917@gmail.com>
2026-06-29 15:35:39 -0400

    config changes2

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
-import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = st.secrets.get("GOOGLE_API_KEY", os.getenv("GOOGLE_API_KEY"))
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+try:
+    import streamlit as st
+
+    if "GOOGLE_API_KEY" in st.secrets:
+        GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+except Exception:
+    pass
